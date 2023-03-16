97224e6d8094eabc0c23bcfc5b00846426675529
unknown <skovalov777@gmail.com>
2023-03-16 12:19:11 +0200

    delete usenavigate

--- src/components/VariableList/VariableList.tsx
+++ src/components/VariableList/VariableList.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useFetchArticleById } from '../../hooks/useFetchVariables';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import RobotPreloader from '@avtopro/preloader/dist/index';
 import './VariableList.css';
 
